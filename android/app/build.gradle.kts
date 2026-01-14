cbd4a42e2c349b130e3c72c78b454d3908639c18
PistonShot1 <nandaprian7@gmail.com>
2026-01-14 20:38:22 +0800

    chore: android config & include debug key for development & project config & permissions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,23 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 
+val debugKeystoreProperties = Properties()
+val debugKeystorePropertiesFile = rootProject.file("debug.key.properties")
+if (debugKeystorePropertiesFile.exists()) {
+    debugKeystoreProperties.load(FileInputStream(debugKeystorePropertiesFile))
+}
 android {
     namespace = "com.zetrix.fluttertest"
     compileSdk = flutter.compileSdkVersion
@@ -30,11 +43,32 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+        getByName("debug") {
+            keyAlias = debugKeystoreProperties["keyAlias"] as String
+            keyPassword = debugKeystoreProperties["keyPassword"] as String
+            storeFile = debugKeystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = debugKeystoreProperties["storePassword"] as String
+        }
+    }
     buildTypes {
+        debug {
+            // applicationIdSuffix = ".debug"
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
